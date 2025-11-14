e7f2e6ce6c72c8cfceb2a46eb00df69ae96c9106
Basant Kumar Mohanty <basantmohanty7@gmail.com>
2025-11-14 13:00:34 +0530

    hello

--- test.py
+++ test.py
@@ -1,2 +1,15 @@
-name = input("Enter your name: ")
-print(f"Hello, {name}! Python is working.")
+import unittest
+import calculator   # the file you are testing
+
+class TestCalculator(unittest.TestCase):
+
+    def test_add(self):
+        self.assertEqual(calculator.add(2, 3), 5)
+        self.assertEqual(calculator.add(-1, 1), 0)
+
+    def test_multiply(self):
+        self.assertEqual(calculator.multiply(2, 4), 8)
+        self.assertEqual(calculator.multiply(0, 10), 0)
+
+if __name__ == "__main__":
+    unittest.main()
